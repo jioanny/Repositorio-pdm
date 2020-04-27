c9f66a65b7b55722f589b8e7667a93aa38a65751
unknown <jioannysaid@gmail.com>
2020-04-27 01:07:31 -0300

    Create a new project

---
+++ ProjetoTesteRepositorio/app/src/main/java/com/example/projetotesterepositorio/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.projetotesterepositorio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
